dfaad33f83b9a95663dab39f2a5280dd7d3276fe
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2020-01-02 20:47:40 +0000

    add first example of end-to-end testing an API for chapter 2

---
+++ chapter2/4_end_to_end_tests/1_http_api_tests/server.js
@@ -0,0 +1,23 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+
+const app = new Koa();
+const router = new Router();
+
+const carts = new Map();
+
+router.get("/carts/:username/items", ctx => {
+  const cart = carts.get(ctx.params.username);
+  cart ? (ctx.body = cart) : (ctx.status = 404);
+});
+
+router.post("/carts/:username/items/:item", ctx => {
+  const { username, item } = ctx.params;
+  const newItems = (carts.get(username) || []).concat(item);
+  carts.set(username, newItems);
+  ctx.body = newItems;
+});
+
+app.use(router.routes());
+
+app.listen(3000);
---
+++ chapter2/4_end_to_end_tests/1_http_api_tests/server.test.js
@@ -0,0 +1,25 @@
+require("./server");
+const fetch = require("isomorphic-fetch");
+
+const apiRoot = "http://localhost:3000";
+
+const addItem = (username, item) => {
+  return fetch(`${apiRoot}/carts/${username}/items/${item}`, {
+    method: "POST"
+  });
+};
+
+const getItems = username => {
+  return fetch(`${apiRoot}/carts/${username}/items`, { method: "GET" });
+};
+
+test("adding items to a cart", async () => {
+  const initialItemsResponse = await getItems("lucas");
+  expect(initialItemsResponse.status).toEqual(404);
+
+  const addItemResponse = await addItem("lucas", "cheesecake");
+  expect(await addItemResponse.json()).toEqual(["cheesecake"]);
+
+  const finalItemsResponse = await getItems("lucas");
+  expect(await finalItemsResponse.json()).toEqual(["cheesecake"]);
+});
