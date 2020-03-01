a4eab810075debdeed7174cfd0207d5966d4e41a
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2020-03-01 17:45:11 +0000

    better example for assertion checks

--- chapter3/2_writing_good_assertions/1_assertion_checks/inventoryController.test.js
+++ chapter3/2_writing_good_assertions/1_assertion_checks/inventoryController.test.js
@@ -3,11 +3,13 @@ const { inventory, addToInventory } = require("./inventoryController");
 beforeEach(() => inventory.set("cheesecake", 0));
 
 test("cancels operation for invalid quantities", () => {
-  expect.hasAssertions();
+  expect.assertions(2);
 
   try {
     addToInventory("cheesecake", "not a number");
   } catch (e) {
     expect(inventory.get("cheesecake")).toBe(0);
   }
+
+  expect(Array.from(inventory.entries())).toHaveLength(1);
 });
