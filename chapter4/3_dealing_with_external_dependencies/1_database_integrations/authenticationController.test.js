cf3a246cf2ee5c995b6a1668c5a54d889b085944
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2020-03-26 09:17:07 +0000

    database integrations first examples

---
+++ chapter4/3_dealing_with_external_dependencies/1_database_integrations/authenticationController.test.js
@@ -0,0 +1,70 @@
+const { db } = require("./dbConnection");
+const crypto = require("crypto");
+const {
+  hashPassword,
+  credentialsAreValid,
+  authenticationMiddleware
+} = require("./authenticationController");
+
+beforeEach(() => db("users").truncate());
+
+describe("hashPassword", () => {
+  test("hashing passwords", () => {
+    const plainTextPassword = "password_example";
+    const hash = crypto.createHash("sha256");
+    hash.update(plainTextPassword);
+    const expectedHash = hash.digest("hex");
+    expect(hashPassword(plainTextPassword)).toBe(expectedHash);
+  });
+});
+
+describe("credentialsAreValid", () => {
+  test("validating credentials", async () => {
+    await db("users").insert({
+      username: "test_user",
+      email: "test_user@example.org",
+      passwordHash: hashPassword("a_password")
+    });
+
+    expect(await credentialsAreValid("test_user", "a_password")).toBe(true);
+  });
+});
+
+describe("authenticationMiddleware", () => {
+  test("returning an error if the credentials are not valid", async () => {
+    const fakeAuth = Buffer.from("invalid:credentials").toString("base64");
+    const ctx = {
+      request: {
+        headers: { authorization: `Basic ${fakeAuth}` }
+      }
+    };
+
+    const next = jest.fn();
+    await authenticationMiddleware(ctx, next);
+    expect(next.mock.calls).toHaveLength(0);
+    expect(ctx).toEqual({
+      ...ctx,
+      status: 401,
+      body: { message: "please provide valid credentials" }
+    });
+  });
+
+  test("authenticating properly", async () => {
+    await db("users").insert({
+      username: "test_user",
+      email: "test_user@example.org",
+      passwordHash: hashPassword("a_password")
+    });
+
+    const validAuth = Buffer.from("test_user:a_password").toString("base64");
+    const ctx = {
+      request: {
+        headers: { authorization: `Basic ${validAuth}` }
+      }
+    };
+
+    const next = jest.fn();
+    await authenticationMiddleware(ctx, next);
+    expect(next.mock.calls).toHaveLength(1);
+  });
+});
---
+++ chapter4/3_dealing_with_external_dependencies/1_database_integrations/migrations/20200325082401_initial_schema.js
@@ -0,0 +1,29 @@
+exports.up = async knex => {
+  await knex.schema.createTable("users", table => {
+    table.increments("id");
+    table.string("username");
+    table.unique("username");
+    table.string("email");
+    table.string("passwordHash");
+  });
+
+  await knex.schema.createTable("carts_items", table => {
+    table.integer("userId").references("users.id");
+    table.string("itemName");
+    table.unique("itemName");
+    table.integer("quantity");
+  });
+
+  await knex.schema.createTable("inventory", table => {
+    table.increments("id");
+    table.string("itemName");
+    table.unique("itemName");
+    table.integer("quantity");
+  });
+};
+
+exports.down = async knex => {
+  await knex.schema.dropTable("inventory");
+  await knex.schema.dropTable("carts");
+  await knex.schema.dropTable("users");
+};
