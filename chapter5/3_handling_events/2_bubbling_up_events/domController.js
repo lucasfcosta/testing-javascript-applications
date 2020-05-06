35d6cae0074f9ebc6822af8fa0f09cbf886cd82e
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2020-05-06 19:25:35 +0100

    add examples for bubbling up events

---
+++ chapter5/3_handling_events/2_bubbling_up_events/domController.js
@@ -0,0 +1,64 @@
+const { addItem, data } = require("./inventoryController");
+const formState = { hasValidItemName: false };
+
+const updateItemList = inventory => {
+  const inventoryList = window.document.getElementById("item-list");
+
+  // Clears the list
+  inventoryList.innerHTML = "";
+
+  Object.entries(inventory).forEach(([itemName, quantity]) => {
+    const listItem = window.document.createElement("li");
+    listItem.innerHTML = `${itemName} - Quantity: ${quantity}`;
+
+    if (quantity < 5) {
+      listItem.className = "almost-soldout";
+    }
+
+    inventoryList.appendChild(listItem);
+  });
+
+  const inventoryContents = JSON.stringify(inventory);
+  const p = window.document.createElement("p");
+  p.innerHTML = `The inventory has been updated - ${inventoryContents}`;
+
+  window.document.body.appendChild(p);
+};
+
+const handleAddItem = event => {
+  // Prevent the page from reloading as it would by default
+  event.preventDefault();
+
+  const { name, quantity } = event.target.elements;
+  addItem(name.value, parseInt(quantity.value, 10));
+
+  updateItemList(data.inventory);
+};
+
+const validItems = ["cheesecake", "apple pie", "carrot cake"];
+const checkFormValues = () => {
+  const itemName = document.querySelector(`input[name="name"]`).value;
+  const quantity = document.querySelector(`input[name="quantity"]`).value;
+
+  const itemNameIsEmpty = itemName === "";
+  const itemNameIsInvalid = !validItems.includes(itemName);
+  const quantityIsEmpty = quantity === "";
+
+  const errorMsg = window.document.getElementById("error-msg");
+  if (itemNameIsEmpty) {
+    errorMsg.innerHTML = "";
+  } else if (itemNameIsInvalid) {
+    errorMsg.innerHTML = `${itemName} is not a valid item.`;
+  } else {
+    errorMsg.innerHTML = `${itemName} is valid!`;
+  }
+
+  const submitButton = document.querySelector(`button[type="submit"]`);
+  if (itemNameIsEmpty || itemNameIsInvalid || quantityIsEmpty) {
+    submitButton.disabled = true;
+  } else {
+    submitButton.disabled = false;
+  }
+};
+
+module.exports = { updateItemList, handleAddItem, checkFormValues };
---
+++ chapter5/3_handling_events/2_bubbling_up_events/domController.test.js
@@ -0,0 +1,96 @@
+const fs = require("fs");
+const initialHtml = fs.readFileSync("./index.html");
+const { getByText, screen } = require("@testing-library/dom");
+
+const {
+  updateItemList,
+  handleAddItem,
+  checkFormValues
+} = require("./domController");
+
+beforeEach(() => {
+  document.body.innerHTML = initialHtml;
+});
+
+describe("updateItemList", () => {
+  test("updates the DOM with the inventory items", () => {
+    const inventory = {
+      cheesecake: 5,
+      "apple pie": 2,
+      "carrot cake": 6
+    };
+    updateItemList(inventory);
+
+    const itemList = document.getElementById("item-list");
+    expect(itemList.childNodes).toHaveLength(3);
+
+    expect(getByText(itemList, "cheesecake - Quantity: 5")).toBeInTheDocument();
+    expect(getByText(itemList, "apple pie - Quantity: 2")).toBeInTheDocument();
+    expect(
+      getByText(itemList, "carrot cake - Quantity: 6")
+    ).toBeInTheDocument();
+  });
+
+  test("highlighting in red elements whose quantity is below five", () => {
+    const inventory = { cheesecake: 5, "apple pie": 2, "carrot cake": 6 };
+    updateItemList(inventory);
+
+    expect(screen.getByText("apple pie - Quantity: 2")).toHaveStyle({
+      color: "red"
+    });
+  });
+
+  test("adding a paragraph indicating what was the update", () => {
+    const inventory = { cheesecake: 5, "apple pie": 2 };
+    updateItemList(inventory);
+
+    expect(
+      screen.getByText(
+        `The inventory has been updated - ${JSON.stringify(inventory)}`
+      )
+    ).toBeTruthy();
+  });
+});
+
+describe("handleAddItem", () => {
+  test("adding items to the page", () => {
+    const event = {
+      preventDefault: jest.fn(),
+      target: {
+        elements: {
+          name: { value: "cheesecake" },
+          quantity: { value: "6" }
+        }
+      }
+    };
+
+    handleAddItem(event);
+
+    // Checking if the form's default reload is prevent
+    expect(event.preventDefault.mock.calls).toHaveLength(1);
+
+    const itemList = document.getElementById("item-list");
+    expect(getByText(itemList, "cheesecake - Quantity: 6")).toBeInTheDocument();
+  });
+});
+
+describe("checkFormValues", () => {
+  test("entering valid item values", () => {
+    document.querySelector(`input[name="name"]`).value = "cheesecake";
+    document.querySelector(`input[name="quantity"]`).value = "1";
+    checkFormValues();
+    expect(screen.getByText("Add to inventory")).toBeEnabled();
+  });
+
+  test("entering invalid item names", () => {
+    document.querySelector(`input[name="name"]`).value = "invalid";
+    document.querySelector(`input[name="quantity"]`).value = "1";
+    checkFormValues();
+    expect(screen.getByText("Add to inventory")).toBeDisabled();
+
+    document.querySelector(`input[name="name"]`).value = "cheesecake";
+    document.querySelector(`input[name="quantity"]`).value = "";
+    checkFormValues();
+    expect(screen.getByText("Add to inventory")).toBeDisabled();
+  });
+});
