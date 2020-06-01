3dded56b128153bf3b9f49e60d75723132e65ae1
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2020-06-01 09:01:34 +0100

    fix misassignment on inventoryController test on chapter 5 - history api section

--- chapter5/4_testing_and_browser_apis/2_history_api/inventoryController.test.js
+++ chapter5/4_testing_and_browser_apis/2_history_api/inventoryController.test.js
@@ -2,7 +2,7 @@ const { addItem, data } = require("./inventoryController");
 
 describe("addItem", () => {
   test("adding new items to the inventory", () => {
-    data.inventory.items = [];
+    data.inventory = {};
     addItem("cheesecake", 5);
     expect(data.inventory.cheesecake).toBe(5);
   });
