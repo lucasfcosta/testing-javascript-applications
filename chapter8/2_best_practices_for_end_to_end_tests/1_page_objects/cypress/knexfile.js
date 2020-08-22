a1500268cfafe5126cb9eafa50dae89a6a77954d
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2020-08-22 15:19:25 +0100

    remove unused cypress knex environment

--- chapter8/2_best_practices_for_end_to_end_tests/1_page_objects/cypress/knexfile.js
+++ chapter8/2_best_practices_for_end_to_end_tests/1_page_objects/cypress/knexfile.js
@@ -3,10 +3,5 @@ module.exports = {
     client: "sqlite3",
     connection: { filename: "../../server/dev.sqlite" },
     useNullAsDefault: true
-  },
-  cypress: {
-    client: "sqlite3",
-    connection: { filename: "../../server/cypress.sqlite" },
-    useNullAsDefault: true
   }
 };
