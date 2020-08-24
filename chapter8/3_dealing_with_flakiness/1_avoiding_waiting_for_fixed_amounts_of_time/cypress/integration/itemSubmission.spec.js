e1f7472e296061514ebb8011a2a679e968256f3d
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2020-08-24 06:55:25 +0100

    add examples on how to avoid waiting for fixed amounts of times on e2e tests

---
+++ chapter8/3_dealing_with_flakiness/1_avoiding_waiting_for_fixed_amounts_of_time/cypress/integration/itemSubmission.spec.js
@@ -0,0 +1,54 @@
+import { InventoryManagement } from "../pageObjects/inventoryManagement";
+
+describe("item submission", () => {
+  beforeEach(() => cy.task("emptyInventory"));
+
+  it("can add items through the form", () => {
+    InventoryManagement.visit();
+    InventoryManagement.addItem("cheesecake", "10");
+    InventoryManagement.findItemEntry("cheesecake", "10");
+  });
+
+  it("can update an item's quantity", () => {
+    cy.task("seedItem", { itemName: "cheesecake", quantity: 5 });
+    InventoryManagement.visit();
+    InventoryManagement.addItem("cheesecake", "10");
+    InventoryManagement.findItemEntry("cheesecake", "15");
+  });
+
+  it("can undo submitted items", () => {
+    InventoryManagement.visit();
+    InventoryManagement.findAction({});
+
+    cy.window().then(({ handleAddItem }) => handleAddItem("cheesecake", 10));
+    InventoryManagement.findAction({ cheesecake: 10 });
+
+    cy.window().then(({ handleAddItem }) => handleAddItem("cheesecake", 5));
+    InventoryManagement.findAction({ cheesecake: 15 });
+
+    InventoryManagement.undo();
+    InventoryManagement.findItemEntry("cheesecake", "10");
+  });
+
+  it("saves each submission to the action log", () => {
+    InventoryManagement.visit();
+    InventoryManagement.addItem("cheesecake", "10");
+    InventoryManagement.addItem("cheesecake", "5");
+    InventoryManagement.undo();
+    InventoryManagement.findItemEntry("cheesecake", "10");
+
+    InventoryManagement.findAction({});
+    InventoryManagement.findAction({ cheesecake: 10 });
+    InventoryManagement.findAction({ cheesecake: 15 });
+    InventoryManagement.findAction({ cheesecake: 10 });
+  });
+
+  describe("given a user enters an invalid item name", () => {
+    it("disables the form's submission button", () => {
+      InventoryManagement.visit();
+      InventoryManagement.enterItemName("boat");
+      InventoryManagement.enterQuantity(10);
+      InventoryManagement.getSubmitButton().should("be.disabled");
+    });
+  });
+});
---
+++ chapter8/3_dealing_with_flakiness/1_avoiding_waiting_for_fixed_amounts_of_time/cypress/support/commands.js
@@ -0,0 +1,10 @@
+import "cypress-wait-until";
+
+Cypress.Commands.add("addItem", (itemName, quantity) => {
+  return cy.request({
+    url: `http://localhost:3000/inventory/${itemName}`,
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ quantity })
+  });
+});
