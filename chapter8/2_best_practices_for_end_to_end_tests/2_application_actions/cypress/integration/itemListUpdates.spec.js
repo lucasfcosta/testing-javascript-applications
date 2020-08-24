01904c494aeb7140542b9ce2415edc78a1703cc1
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2020-08-24 06:55:01 +0100

    add examples for application actions on chapter 8

---
+++ chapter8/2_best_practices_for_end_to_end_tests/2_application_actions/cypress/integration/itemListUpdates.spec.js
@@ -0,0 +1,27 @@
+import { InventoryManagement } from "../pageObjects/inventoryManagement";
+
+describe("item list updates", () => {
+  beforeEach(() => cy.task("emptyInventory"));
+
+  describe("when the application loads for the first time", () => {
+    it("loads the initial list of items", () => {
+      cy.addItem("cheesecake", 2);
+      cy.addItem("apple pie", 5);
+      cy.addItem("carrot cake", 96);
+      cy.visit("http://localhost:8080");
+
+      InventoryManagement.findItemEntry("cheesecake", "2");
+      InventoryManagement.findItemEntry("apple pie", "5");
+      InventoryManagement.findItemEntry("carrot cake", "96");
+    });
+  });
+
+  describe("as other users add items", () => {
+    it("updates the item list", () => {
+      cy.visit("http://localhost:8080");
+      InventoryManagement.findAction({});
+      cy.addItem("cheesecake", 22);
+      InventoryManagement.findItemEntry("cheesecake", "22");
+    });
+  });
+});
---
+++ chapter8/2_best_practices_for_end_to_end_tests/2_application_actions/cypress/integration/itemSubmission.spec.js
@@ -0,0 +1,56 @@
+import { InventoryManagement } from "../pageObjects/inventoryManagement";
+
+describe("item submission", () => {
+  beforeEach(() => cy.task("emptyInventory"));
+
+  it("can add items through the form", () => {
+    InventoryManagement.visit();
+    InventoryManagement.addItem("cheesecake", "10");
+    InventoryManagement.findItemEntry("cheesecake", "10");
+  });
+
+  it("can update an item's quantity", () => {
+    cy.task("seedItem", { itemName: "cheesecake", quantity: 5 });
+    InventoryManagement.visit();
+    InventoryManagement.addItem("cheesecake", "10");
+    InventoryManagement.findItemEntry("cheesecake", "15");
+  });
+
+  it("can undo submitted items", () => {
+    InventoryManagement.visit();
+    cy.wait(1000);
+
+    InventoryManagement.findAction({});
+    cy.window().then(({ handleAddItem }) => handleAddItem("cheesecake", 10));
+    cy.wait(1000);
+
+    InventoryManagement.findItemEntry("cheesecake", "10");
+    cy.window().then(({ handleAddItem }) => handleAddItem("cheesecake", 5));
+    cy.wait(1000);
+
+    InventoryManagement.undo();
+    InventoryManagement.findItemEntry("cheesecake", "10");
+  });
+
+  it("saves each submission to the action log", () => {
+    InventoryManagement.visit();
+    InventoryManagement.addItem("cheesecake", "10");
+    InventoryManagement.addItem("cheesecake", "5");
+    InventoryManagement.undo();
+    InventoryManagement.findItemEntry("cheesecake", "10");
+
+    InventoryManagement.findAction({});
+    InventoryManagement.findAction({ cheesecake: 10 });
+    InventoryManagement.findAction({ cheesecake: 15 });
+    InventoryManagement.findAction({ cheesecake: 10 });
+  });
+
+  describe("given a user enters an invalid item name", () => {
+    it("disables the form's submission button", () => {
+      InventoryManagement.visit();
+      InventoryManagement.enterItemName("boat");
+      InventoryManagement.enterQuantity(10);
+      InventoryManagement.getSubmitButton().should("be.disabled");
+    });
+  });
+});
