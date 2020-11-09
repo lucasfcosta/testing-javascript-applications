89b5dc442e6536b55f3e0841411607adabe51ef0
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2020-11-09 07:33:50 +0000

    fix chapter 4 timestamp check matcher

--- chapter3/2_writing_good_assertions/4_manual_assertions/inventoryController.test.js
+++ chapter3/2_writing_good_assertions/4_manual_assertions/inventoryController.test.js
@@ -4,5 +4,5 @@ test("generatedAt in the past", () => {
   const result = getInventory();
   const currentTime = Date.now() + 1;
   const isPastTimestamp = result.generatedAt.getTime() <= currentTime;
-  expect(isPastTimestamp).toBeBefore(true);
+  expect(isPastTimestamp).toBe(true);
 });
