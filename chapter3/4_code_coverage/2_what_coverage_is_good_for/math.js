015fb0faa11e25b09044115e788f282fda073370
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2020-11-09 07:51:53 +0000

    fix: new folders for different parts of code coverage in chapter 3

---
+++ chapter3/4_code_coverage/2_what_coverage_is_good_for/math.js
@@ -0,0 +1,7 @@
+function sumOrDivide(a, b) {
+  if (a % 2 === 0 && b % 2 === 0) {
+    return a + b;
+  } else {
+    return a / b;
+  }
+}
---
+++ chapter3/4_code_coverage/2_what_coverage_is_good_for/math.test.js
@@ -0,0 +1,9 @@
+test("sum", () => {
+  sumOrDivide(2, 4);
+  // WARNING: No assertions!
+});
+
+test("multiply", () => {
+  sumOrDivide(2, 6);
+  // WARNING: No assertions!
+});
