7b76c21b41f8709b72ce210d60d1366a654c8082
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2020-11-12 08:20:33 +0000

    update examples storybook version

--- chapter8/4_component_stories/1_stories/.storybook/main.js
+++ chapter8/4_component_stories/1_stories/.storybook/main.js
@@ -3,5 +3,17 @@ module.exports = {
   addons: [
     "@storybook/addon-actions/register",
     "@storybook/addon-knobs/register"
-  ]
+  ],
+  webpackFinal: async config => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          "core-js/modules": "@storybook/core/node_modules/core-js/modules",
+          "core-js/features": "@storybook/core/node_modules/core-js/features"
+        }
+      }
+    };
+  }
 };
--- chapter8/4_component_stories/2_documentation/.storybook/main.js
+++ chapter8/4_component_stories/2_documentation/.storybook/main.js
@@ -7,5 +7,17 @@ module.exports = {
       name: "@storybook/addon-docs",
       options: { configureJSX: true }
     }
-  ]
+  ],
+  webpackFinal: async config => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          "core-js/modules": "@storybook/core/node_modules/core-js/modules",
+          "core-js/features": "@storybook/core/node_modules/core-js/features"
+        }
+      }
+    };
+  }
 };
