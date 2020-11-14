b80575c75fb5f83423973635aaa13acc209165a3
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2020-11-14 15:08:10 +0000

    add key to action log items on chapter 3 style tests

--- chapter8/3_testing_styles/3_css_in_js_snapshots/ActionLog.jsx
+++ chapter8/3_testing_styles/3_css_in_js_snapshots/ActionLog.jsx
@@ -5,10 +5,10 @@ export const ActionLog = ({ actions }) => {
     <div data-testid="action-log">
       <h2>Action Log</h2>
       <ul>
-        {actions.map(({ time, message, data }) => {
+        {actions.map(({ time, message, data }, i) => {
           const date = new Date(time).toUTCString();
           return (
-            <li>
+            <li key={i}>
               Date: {date} - Message: {message} - Data: {JSON.stringify(data)}
             </li>
           );
